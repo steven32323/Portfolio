74c8547abb80e2cb992460803a52001859cebf06
Steven32323 <stevo323231@gmail.com>
2023-01-11 14:57:23 +0000

    properly added event delegation for smooth scrolling

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 
 // Scrolling functions
 
-document.querySelectorAll(".nav-btn").forEach(function (e) {
-  e.addEventListener("click", function (e) {
-    e.preventDefault();
-    const id = this.getAttribute("href");
-    // console.log(id);
+document.querySelector(".nav-links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav-btn")) {
+    const id = e.target.getAttribute("href");
+    console.log(id);
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-  });
+  }
 });
