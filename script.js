393580b6f0312aa21b5791a7145d09bb3aac991b
Steven32323 <stevo323231@gmail.com>
2023-01-12 21:07:40 +0000

    Added fading/unfading of links on hover

--- script.js
+++ script.js
@@ -8,7 +8,23 @@ document.querySelector(".nav-links").addEventListener("click", function (e) {
   // Matching strategy
   if (e.target.classList.contains("nav-btn")) {
     const id = e.target.getAttribute("href");
-    console.log(id);
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+// fading links on hover
+
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav-btn")) {
+    const link = e.target;
+    const siblings = link.closest(".links").querySelectorAll(".nav-btn");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
+};
+
+// passing "argument" into handler
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
