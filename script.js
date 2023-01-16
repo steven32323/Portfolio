affd0fda19bb76df6ce25ade45fdc2abfda631a2
Steven32323 <stevo323231@gmail.com>
2023-01-16 09:06:45 +0000

    added a sticky navigation bar

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
-"use strict";
-
+'use strict';
 // Scrolling functions
 
-document.querySelector(".nav-links").addEventListener("click", function (e) {
+document.querySelector('.nav-links').addEventListener('click', function (e) {
   e.preventDefault();
 
   // Matching strategy
-  if (e.target.classList.contains("nav-btn")) {
-    const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  if (e.target.classList.contains('nav-btn')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
 
 // fading links on hover
 
 const handleHover = function (e) {
-  if (e.target.classList.contains("nav-btn")) {
+  if (e.target.classList.contains('nav-btn')) {
     const link = e.target;
-    const siblings = link.closest(".links").querySelectorAll(".nav-btn");
+    const siblings = link.closest('.links').querySelectorAll('.nav-btn');
 
-    siblings.forEach((el) => {
+    siblings.forEach(el => {
       if (el !== link) el.style.opacity = this;
     });
   }
 };
 
 // passing "argument" into handler
-nav.addEventListener("mouseover", handleHover.bind(0.5));
-nav.addEventListener("mouseout", handleHover.bind(1));
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky navigation bar
+
+const header = document.querySelector('.overlay');
+const navBar = document.querySelector('.nav-bar');
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) {
+    navBar.classList.add('sticky');
+  } else navBar.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+});
+headerObserver.observe(header);
